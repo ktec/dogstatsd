c13a5eaa210933641adb8d9d5ce9af4b501d3307
Keith Salisbury <keithsalisbury@gmail.com>
2017-02-15 22:26:32 +0000

    Some simple clean up and add docs
    A few bits of documentation added, and some code restructuring in
    preparation for the next stage.

--- lib/dogstatsd.ex
+++ lib/dogstatsd.ex
@@ -1,45 +1,113 @@
 defmodule Dogstatsd do
-  def set(metric, value), do: "#{metric}:#{value}|s"
-  def set(metric, value, ts),
-    do: join(set(metric, value), tags(ts), "|")
+  @moduledoc """
+  A Dogstatsd client
+  """
 
-  def counter(metric, value), do: "#{metric}:#{value}|c"
-  def counter(metric, value, ts),
-    do: join(counter(metric, value), tags(ts), "|")
+  alias Dogstatsd.Metric, as: M
+  alias Dogstatsd.Batch, as: B
 
-  def gauge(metric, value), do: "#{metric}:#{value}|g"
-  def gauge(metric, value, ts),
-    do: join(gauge(metric, value), tags(ts), "|")
+  @doc """
+  Sends an arbitary set value for the given stat to the statsd server.
 
-  def histogram(metric, value), do: "#{metric}:#{value}|h"
-  def histogram(metric, value, ts),
-    do: join(histogram(metric, value), tags(ts), "|")
+  ## Examples
 
-  def increment(metric), do: "#{metric}:1|c"
-  def decrement(metric), do: "#{metric}:-1|c"
+      iex> Dogstatsd.set("deployment", DEPLOYMENT_EVENT_CODE)
+      "vdevice.deployment:001|s"
 
-  def batch([]), do: ""
-  def batch([h]), do: metric(h)
-  # def batch([h|t]), do: join(batch(t), metric(h), "\n")
-  def batch(examples) do
-    Enum.reduce(examples, "", fn
-      (metric, acc) -> join(metric(metric), acc, "\n")
-    end)
-  end
+  """
+  @spec set(metric :: binary, value :: integer) :: String.t
 
-  def metric({<<>>, _, _}), do: ""
-  def metric({metric, value, "s"}), do: set(metric, value)
-  def metric({metric, value, "c"}), do: counter(metric, value)
-  def metric({metric, value, "g"}), do: gauge(metric, value)
-  def metric({metric, value, "h"}), do: histogram(metric, value)
+  defdelegate set(metric, value), to: M
+  defdelegate set(metric, value, tags), to: M
 
-  def join("", b, _), do: b
-  def join(a, "", _), do: a
-  def join(a, b, c), do: a <> c <> b
+  @doc """
+  Sends an arbitrary count for the given stat to the statsd server.
 
-  def tags([]), do: ""
-  def tags([""]), do: ""
-  def tags([<<>>]), do: ""
-  def tags([<<0>>]), do: ""
-  def tags(ts), do: "#" <> Enum.join(ts, ",")
+  ## Examples
+
+      iex> Dogstatsd.counter("user.count", 100)
+      "user.count:100|c"
+
+  """
+  @spec counter(metric :: binary, value :: integer) :: String.t
+
+  defdelegate counter(metric, value), to: M
+  defdelegate counter(metric, value, tags), to: M
+
+  @doc """
+  Sends an arbitary gauge value for the given stat to the statsd server.
+
+  This is useful for recording things like available disk space, memory usage,
+  and the like, which have different semantics than counters.
+
+  ## Examples
+
+      iex> Dogstatsd.gauge("user.count", 100)
+      "user.count:100|c"
+
+  """
+  @spec gauge(metric :: binary, value :: integer) :: String.t
+
+  defdelegate gauge(metric, value), to: M
+  defdelegate gauge(metric, value, tags), to: M
+
+  @doc """
+  Sends a value to be tracked as a histogram to the statsd server.
+
+  ## Examples
+
+      iex> Dogstatsd.histogram("user.count", 100)
+      "user.count:100|c"
+
+  """
+  @spec histogram(metric :: binary, value :: integer) :: String.t
+
+  defdelegate histogram(metric, value), to: M
+  defdelegate histogram(metric, value, tags), to: M
+
+  @doc """
+  Sends an increment count for the given stat to the statsd server.
+
+  ## Examples
+
+      iex> Dogstatsd.increment("user.count")
+      "user.count:1|c"
+      iex> Dogstatsd.increment("user.count", 10)
+      "user.count:10|c"
+
+  """
+  @spec increment(metric :: binary, value :: integer) :: String.t
+
+  defdelegate increment(metric, value), to: M
+
+  @doc """
+  Sends an decrement count for the given stat to the statsd server.
+
+  ## Examples
+
+      iex> Dogstatsd.decrement("user.count")
+      "user.count:1|c"
+      iex> Dogstatsd.decrement("user.count", 10)
+      "user.count:10|c"
+
+  """
+  @spec decrement(metric :: binary, value :: integer) :: String.t
+
+  defdelegate decrement(metric, value), to: M
+
+  @doc """
+  Sends an decrement count for the given stat to the statsd server.
+
+  ## Examples
+
+      iex> Dogstatsd.batch({"keith", 123, "s"}, {"tanya", 234, "h"})
+      "keith:123|s\ntanya:234|h"
+
+  """
+  @spec batch(Enum.t) :: String.t
+
+  defdelegate batch(examples), to: B
+  defdelegate metric(metric), to: M
+  defdelegate join(a, b, c), to: M
+  defdelegate tags(tags), to: M
 end
--- test/dogstatsd_test.exs
+++ test/dogstatsd_test.exs
@@ -2,33 +2,43 @@ defmodule DogstatsdTest do
   use ExUnit.Case
   use EQC.ExUnit
   # doctest Dogstatsd
-  require Dogstatsd
+  alias Dogstatsd, as: D
+
+  def string, do: utf8()
 
   @tag numtests: 1000
   property "set metric|value" do
     #   one_of(generate_metric(64)),
     #   one_of(generate_value(128))
-    forall [metric, value] <- [utf8, int] do
-      ensure Dogstatsd.set(metric, value) == "#{metric}:#{value}|s"
-      ensure Dogstatsd.counter(metric, value) == "#{metric}:#{value}|c"
-      ensure Dogstatsd.gauge(metric, value) == "#{metric}:#{value}|g"
-      ensure Dogstatsd.histogram(metric, value) == "#{metric}:#{value}|h"
-      ensure Dogstatsd.increment(metric) == "#{metric}:1|c"
-      ensure Dogstatsd.decrement(metric) == "#{metric}:-1|c"
+    forall [metric, value] <- [string(), int()] do
+      ensure D.set(metric, value) == "#{metric}:#{value}|s"
+      ensure D.counter(metric, value) == "#{metric}:#{value}|c"
+      ensure D.gauge(metric, value) == "#{metric}:#{value}|g"
+      ensure D.histogram(metric, value) == "#{metric}:#{value}|h"
+    end
+  end
+
+  @tag numtests: 1000
+  property "increment metric|amount" do
+    forall [metric, amount] <- [string(), nat()] do
+      implies amount > 0 do
+        ensure D.increment(metric, amount) == "#{metric}:#{amount}|c"
+        ensure D.decrement(metric, amount) == "#{metric}:-#{amount}|c"
+      end
     end
   end
 
   @tag numtests: 1000
   property "set metric|value with tags" do
-    forall [metric, value, tags] <- [utf8, int, list(utf8)] do
-      ensure Dogstatsd.set(metric, value, tags)
-        ==  Dogstatsd.set(metric, value) |> with_tags(tags)
-      ensure Dogstatsd.counter(metric, value, tags)
-        == Dogstatsd.counter(metric, value) |> with_tags(tags)
-      ensure Dogstatsd.gauge(metric, value, tags)
-        == Dogstatsd.gauge(metric, value) |> with_tags(tags)
-      ensure Dogstatsd.histogram(metric, value, tags)
-        == Dogstatsd.histogram(metric, value) |> with_tags(tags)
+    forall [metric, value, tags] <- [string(), int(), list(string())] do
+      ensure D.set(metric, value, tags)
+        ==  D.set(metric, value) |> with_tags(tags)
+      ensure D.counter(metric, value, tags)
+        == D.counter(metric, value) |> with_tags(tags)
+      ensure D.gauge(metric, value, tags)
+        == D.gauge(metric, value) |> with_tags(tags)
+      ensure D.histogram(metric, value, tags)
+        == D.histogram(metric, value) |> with_tags(tags)
     end
   end
 
@@ -38,20 +48,20 @@ defmodule DogstatsdTest do
    [{"keith",123}, {"tanya", 234}] == "keith:123|s\ntanya:234|h"
   """
   property "batch metric|value" do
-    forall examples <- list({utf8, int, elements(["s", "c", "g", "h"])}) do
+    forall examples <- list({utf8(), int(), elements(["s", "c", "g", "h"])}) do
 
       expected = Enum.reduce(examples, "", fn
         (metric, acc) ->
-          join(Dogstatsd.metric(metric), acc, "\n")
+          join(D.metric(metric), acc, "\n")
       end)
 
-      ensure Dogstatsd.batch(examples) == expected
+      ensure D.batch(examples) == expected
     end
   end
 
-  defp join(a, b, c), do: Dogstatsd.join(a, b, c)
+  defp join(a, b, c), do: D.join(a, b, c)
 
-  defp with_tags(something, tags), do: join(something, Dogstatsd.tags(tags), "|")
+  defp with_tags(something, tags), do: join(something, D.tags(tags), "|")
 
   # defp generate_metric(length) do
   #   :crypto.strong_rand_bytes(length)
